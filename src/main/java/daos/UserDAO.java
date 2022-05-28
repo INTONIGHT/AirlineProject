ed331815df9202de17685f61e559c3e6a4a29dba
INTONIGHT <tyler.throop@gmail.com>
2022-05-27 22:12:12 -0700

    Just some code cleanup will continue work another day as this is just for practice

--- src/main/java/daos/UserDAO.java
+++ src/main/java/daos/UserDAO.java
@@ -3,6 +3,7 @@ package daos;
 import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,6 +12,7 @@ import models.Reservation;
 
 public class UserDAO {
 	private Connection conn = JDBCConnection.getConnection();
+	private ReservationDAO rdao = new ReservationDAO();
 	
 	public boolean createUser(String username,String password,ArrayList<Reservation> reservations) {
 		String sql = "insert into users values(default,?,?,?);";
@@ -27,12 +29,40 @@ public class UserDAO {
 		}
 		return false;
 	}
-	
+	//returns the reservations of a user by id
+	public ArrayList<Reservation> getReservations(int id){
+		//i believe it would look something like this where you get the values
+		//I am not entirely sure how well SQL can handle storing arrays
+		//assuming that the userid will be the user we want and that
+		//the user table will have a one to many relationship with reservations
+		String sql = "select * from reservations where user_id = ?;";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			ArrayList<Reservation> reservations = new ArrayList<Reservation>(); 
+			while(rs.next()) {
+				Reservation r = rdao.getReservation(id);
+				reservations.add(r);
+				
+			}
+			//with an actual SQL database I could test this. parts of this are more psuedocode but as an example of what you could do
+			return reservations;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 	/**
 	 * the plan is to have two ways to update a user
-	 * The First way will be to let you update the reservations they have
+	 * The First way will be to let you update the reservations they have by adding a reservation specifically
 	 * the second is updating a username or password
 	 * 
 	 */
-	public boolean updateUser()
+	public boolean updateUser(int userid,ArrayList<Reservation> reservations) {
+		
+		
+		return false;
+	}
 }
