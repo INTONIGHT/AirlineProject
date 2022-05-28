9a4de77b58a172539d716269bd691f95bdb17ac1
INTONIGHT <tyler.throop@gmail.com>
2022-05-27 22:10:57 -0700

    adding get reservations to users of note is that some of this is pseudocode so in an actual production scenario you would need to have the database built out and then test what I do with loops etc. This is mostly just for practice and isnt meant to run at the moment. I am refamiliarizzing myself with coding conceptss

--- src/main/java/daos/UserDAO.java
+++ src/main/java/daos/UserDAO.java
@@ -1,5 +1,38 @@
 package daos;
 
-public class UserDAO {
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import connection.JDBCConnection;
+import models.Reservation;
+
+public class UserDAO {
+	private Connection conn = JDBCConnection.getConnection();
+	
+	public boolean createUser(String username,String password,ArrayList<Reservation> reservations) {
+		String sql = "insert into users values(default,?,?,?);";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, username);
+			ps.setString(2, password);
+			ps.setArray(3, (Array) reservations);
+			boolean success = ps.execute();
+			return success;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	/**
+	 * the plan is to have two ways to update a user
+	 * The First way will be to let you update the reservations they have
+	 * the second is updating a username or password
+	 * 
+	 */
+	public boolean updateUser()
 }
